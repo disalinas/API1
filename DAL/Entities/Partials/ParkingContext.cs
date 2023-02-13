8345266fe531b8a2adcf579d781673c622d97b3d
David <davidinaxio@gmail.com>
2023-02-13 21:09:17 +0100

    EF únicamente en capa de datos DAL. Base de datos de prueba en App_Data para simplificar.

---
+++ DAL/Entities/Partials/ParkingContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.SqlServer;
+
+namespace DAL.Entities.Partials
+{
+    public partial class ParkingContext
+    {
+        private static SqlProviderServices instance = SqlProviderServices.Instance;
+    }
+}
